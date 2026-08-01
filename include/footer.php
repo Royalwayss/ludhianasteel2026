733ae997bc1120b100af0b08559540a343ad4a13
Hxtreme <rwpttech@gmail.com>
2026-08-01 13:51:43 +0530

    config

---
+++ include/footer.php
@@ -0,0 +1,218 @@
+     <!-- start footer -->
+     <footer class="footer-bg">
+        
+         <div class="container-fluid">
+
+             <div class="row justify-content-center">
+                 <!-- start footer column -->
+                 <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-5 mt-5" >
+                    <div class="text-start  text-lg-center">
+                     <a href="" class="footer-logo mb-20px sm-mb-15px d-inline-block"><img
+                             src="./images/lsrm/logo-w-b.png" data-at2x="./images/lsrm/logo-w-b.png" alt=""></a>
+                 
+                      <span class="alt-font d-block footer-lsrm-line">Ludhiana Steel Rolling Mills</span>
+                      
+                     <!-- <p class="text-white">LSR Mills was established in 1938, foreseeing the future growth of India’s steel industry.  </p> -->
+                    </div>
+                  
+                 </div>
+                 <!-- end footer column -->
+                 <!-- start footer column -->
+                 <div class="col-12 col-sm-6 col-md-6 col-lg-2 col-xl-3 mb-5 mt-5" >
+                     <span class="alt-font  fs-20 d-block footer-heading mb-10px">Company</span>
+                     <ul>
+                         <li><a href="index.php">Home</a></li>
+                         <li><a href="about.php">About us</a></li>
+                         <!-- <li><a href="products.php">Products</a></li> -->
+                          <li><a href="csr.php">Sustainability</a></li>
+                         <li><a href="infrastructure.php">Infrastructure</a></li>
+                         <li><a href="contact.php">Contact us</a></li>
+                     </ul>
+                 </div>
+                 <!-- end footer column -->
+                 <!-- start footer column -->
+                 <div class="col-12 col-sm-6 col-md-6 col-lg-2 col-xl-2 mb-5 mt-5" >
+                     <span class="alt-font  fs-20 d-block footer-heading mb-10px">Products</span>
+                     <ul>
+                         <!-- <li><a href="products.php">Flat Bars</a></li> -->
+                         <li><a href="products.php">RCS Bars</a></li>
+                         <li><a href="products.php">Round Bars </a></li>
+
+                     </ul>
+                 </div>
+                 <!-- end footer column -->
+                 <!-- start footer column -->
+                 <div  class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 mb-5 mt-5" >
+                  <div class="contact-us">
+                           <span class="alt-font  fs-20 d-block footer-heading mb-10px">Contact Us</span>
+                     <ul>
+                        <li><i class="fa-solid fa-phone ldh-footericon"></i><a href="tel:+91-99149-99222">Phone: +91-99149-99222</a></li>
+                         <li><i class="fa-solid fa-phone ldh-footericon"></i><a href="tel:+91-161-5266000">Phone: +91-161-5266000</a></li>
+                         <li><i class="fa-solid fa-envelope ldh-footericon"></i><a href="mailto:info@ludhianasteel.com">
+                                 info@ludhianasteel.com</a></li>
+                         <li class="d-flex"><i class="fa-solid fa-location-dot ldh-footericon"></i> Opp. Dhandari Railway Station, G.T.
+                             Road, Ludhiana, Punjab, 141014</li>
+                     </ul>
+                    </div>
+                
+                 
+                     <!-- <div class="ldh-footer-effect egeon">
+                         <div class="ldh-footer-buttons">
+                             <a href="#" class="ldh-footer-fb" title="Join us on Facebook">
+                                 <i class="fa-brands fa-facebook"></i>
+                             </a>
+
+                             <a href="#" class="ldh-footer-insta" title="Join us on Instagram">
+                                 <i class="fa-brands fa-instagram"></i>
+                             </a>
+                             <a href="#" class="ldh-footer-in" title="Join us on LinkedIn">
+                                 <i class="fa-brands fa-linkedin"></i>
+                             </a>
+                         </div>
+                     </div> -->
+                 </div>
+                 <!-- end footer column -->
+             </div>
+         </div>
+     </footer>
+     <div class="footer-bottom " style="background-color: #f28322; padding: 10px 0;">
+            <div class="container">
+                <div class="row w-100">
+                    <div class="bottom-column col-md-12 col-sm-12 text-center">
+                        <div class="copyright-text text-white">© 2026, Ludhiana Steel Rolling Mills All Rights Reserved</div>
+                    </div>
+                    <div class="bottom-column col-md-6 col-sm-12 text-center">
+                     <!-- <span class="" style="font-size: 14px; color: #ffffffff; ">Designed By 
+                       <a href="http://royalways.com/" target="_blank" style="color: #ffffffff; font-size: 14px; font-weight:600">Royalways</a>
+              
+                
+                        </span> -->
+                    </div>
+                </div>
+                
+                <!--Scroll to top-->
+                <!--<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon flaticon-right-arrow"></span></div>-->
+             
+                <!--Scroll to top-->
+            </div>
+        </div>
+     <!-- end footer -->
+     <!-- end footer -->
+     <!-- start scroll progress -->
+      <div class="whats-app-btn  d-block">
+         <a href="https://wa.me/+917717304050" target="_blank" aria-label="WhatsApp" class="whats-app-icon">
+             <img src="images/lsrm/whatsapp.png" alt="WhatsApp">
+         </a>
+
+      </div>
+     <div class="scroll-progress d-none d-xxl-block">
+         <a href="#" class="scroll-top" aria-label="scroll">
+             <span class="scroll-text">Scroll</span><span class="scroll-line"><span class="scroll-point"></span></span>
+         </a>
+     </div>
+     <!-- end scroll progress -->
+     <!-- javascript libraries -->
+     <script type="text/javascript" src="js/jquery.js"></script>
+     <script type="text/javascript" src="js/vendors.min.js"></script>
+     <script async defer src="https://maps.googleapis.com/maps/api/js?key=<YOUR_API_KEY>&callback=initMap"></script>
+     <script type="text/javascript" src="js/main.js?v=2.0"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+document.getElementById("myBtn").addEventListener("click", function () {
+    const hiddenItems = document.querySelectorAll(".infra-hidden");
+    const btn = this;
+    const isHidden = hiddenItems[0].style.display === "list-item";
+
+    hiddenItems.forEach(item => {
+        item.style.display = isHidden ? "none" : "list-item";
+    });
+
+    btn.textContent = isHidden ? "Read more" : "Read less";
+});
+</script>
+
+
+
+
+<script>
+// Increase video speed (1 = normal, 1.5 = faster, 2 = double speed)
+document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById("bgVideo");
+    video.playbackRate = 1.5; // Change this number (1.2, 1.5, 2, etc.)
+});
+</script>
+     <!-- tabs applications -->
+      
+  <!-- JS -->
+<!-- JS -->
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = Array.from(document.querySelectorAll('[data-bs-toggle="tab"]'));
+    const panes = Array.from(document.querySelectorAll('.tab-pane'));
+    let currentIndex = 0;
+    let interval;
+
+    function showTab(index) {
+        tabs.forEach(t => t.classList.remove('active'));
+        panes.forEach(p => p.classList.remove('show', 'active'));
+
+        tabs[index].classList.add('active');
+        const targetPane = document.querySelector(tabs[index].getAttribute('href'));
+        if (targetPane) {
+            targetPane.classList.add('show', 'active');
+        }
+    }
+
+    function startAutoSwitch() {
+        interval = setInterval(() => {
+            currentIndex = (currentIndex + 1) % tabs.length;
+            showTab(currentIndex);
+        }, 3000);
+    }
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', (e) => {
+            e.preventDefault();
+            clearInterval(interval);
+            currentIndex = index;
+            showTab(index);
+            setTimeout(startAutoSwitch, 500); // resume after 10s
+        });
+    });
+
+    startAutoSwitch();
+});
+</script>
+
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    const boxes = document.querySelectorAll(".project-box-app");
+    let currentIndex = 0;
+
+    function showNext() {
+        // hide all
+        boxes.forEach(box => box.classList.remove("active"));
+
+        // show current
+        boxes[currentIndex].classList.add("active");
+
+        // move to next
+        currentIndex = (currentIndex + 1) % boxes.length;
+    }
+
+    // first show after 3 sec
+    setTimeout(() => {
+        showNext();
+        setInterval(showNext, 2000);
+    }, 2000);
+});
+</script>
+
+
+     <script>
+  AOS.init();
+</script>
+     </body>
+
+     </html>
---
+++ include/header.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <title>Ludhiana Steel Rolling Mills</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="ThemeZaa">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+
+    <!-- favicon icon -->
+    <link rel="shortcut icon" href="./images/lsrm/logo-remove.png">
+    <!-- <link rel="apple-touch-icon" href="images/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png"> -->
+    <!-- google fonts preconnect -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" href="css/vendors.min.css" />
+    <link rel="stylesheet" href="css/icon.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="demos/ludhiana-steel/style.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+
+<body data-mobile-nav-style="classic">
+    <!-- start header -->
+
+
+    <header>
+
+        <nav class="navbar navbar-expand-lg header-transparent bg-transparent header-reverse "
+            data-header-hover="light">
+            <div class="container mobile-new-width">
+                <div class="col-auto col-lg-2 me-lg-0 me-auto mobile-new-width-logo">
+                    <a class="navbar-brand" href="index.php">
+                        <img style="min-height:110px;" src="./images/lsrm/logo-w-b.png" data-at2x="./images/lsrm/logo-w-b.png" alt=""
+                            class="default-logo">
+                        <img src="./images/lsrm/logo-remove.png" data-at2x="./images/lsrm/logo-remove.png" alt=""
+                            class="alt-logo">
+                        <img src="./images/lsrm/logo-remove.png" data-at2x="./images/lsrm/logo-remove.png" alt=""
+                            class="mobile-logo">
+                    </a>
+                </div>
+                 <div class="col-auto menu-order position-static new-navbar-toggler">
+                    <div class="col-auto col-lg-2 p-0 text-center align-items-center">
+                        <div class="header-icon d-block d-lg-none">
+                            <div class="header-push-button hamburger-push-button icon">
+                                <div class="push-button">
+                                    <span></span>
+                                    <span></span>
+                                    <b
+                                        class="push-button-text text-uppercase text-white position-absolute top-3px left-minus-50px fs-13 fw-600">Menu</b>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav alt-font ls-1px">
+                            <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                            <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+
+                            <li class="nav-item"><a href="products.php" class="nav-link">Products</a></li>
+                            <li class="nav-item"><a href="infrastructure.php" class="nav-link">Infrastructure</a></li>
+                            <li class="nav-item"><a href="csr.php" class="nav-link">Sustainability</a></li>
+                            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto col-lg-2 text-end d-none d-sm-flex">
+
+                </div>
+                <div class="push-menu hamburger-nav header-dark hamburger-menu-simple bg-dark-gray"
+                    style="background-image: url(images/vertical-line-bg-small.svg)">
+                    <span class="close-menu bg-white fs-18"><i class="fa-solid fa-xmark text-dark-gray"></i></span>
+                    <div class="container d-flex flex-column justify-content-center h-100">
+                        <div class="row align-items-center justify-content-center">
+                            <div class="col-lg-8 hamburger-menu menu-list-wrapper no-margin"
+                                data-scroll-options='{ "theme": "light" }'>
+                                <ul class="menu-item-list fw-500 alt-font p-0 text-center text-lg-start">
+                                    <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                                    <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+
+                                    <li class="nav-item"><a href="products.php" class="nav-link">Products</a></li>
+                                    <li class="nav-item"><a href="infrastructure.php"
+                                            class="nav-link">Infrastructure</a></li>
+                                    <li class="nav-item"><a href="csr.php" class="nav-link">Sustainability</a></li>
+                                    <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    <!-- end header -->
